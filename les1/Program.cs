7ff0fa1011e257f2a189214156db15991e297f95
Anastasiya Zayarnaya <anastasiya.zayarnaya@defa.ru>
2021-01-16 13:18:55 +0300

    Lesson1: ДЗ к первому уроку

---
+++ les1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lesson1
+{
+    class Program
+    {
+       static void Main(string[] args)
+       {
+           var name = String.Empty;
+           Console.Write("Представтесь: ");
+           name = Console.ReadLine();
+           Console.WriteLine();
+           Console.WriteLine("Привет, {0}, сегодня {1}", name, DateTime.Now.ToShortDateString());
+           Console.ReadLine();
+        }
+    }
+}
